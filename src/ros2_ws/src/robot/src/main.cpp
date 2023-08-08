203d79129a711b9a6a30b74cdf24edcba2ede1be
mechatronics <mechatronics.sdsu@gmail.com>
2023-08-08 11:54:34 -0700

    Testing - Zix

--- src/ros2_ws/src/robot/src/main.cpp
+++ src/ros2_ws/src/robot/src/main.cpp
@@ -1,8 +1,6 @@
 #include "robot.hpp"
 #define CONFIG_FILE "/home/mechatronics/clean/src/ros2_ws/src/robot/config.json"
 
-
-
 int main(int argc, char** argv)
 {
     std::unique_ptr<Configuration> config = std::make_unique<Configuration>(CONFIG_FILE);
@@ -11,7 +9,7 @@ int main(int argc, char** argv)
     RobotType type = RobotFactory::getType(json_string["robot"]);
     std::unique_ptr<Robot> robot = RobotFactory::CreateRobot(type, *config);
 
-    // robot->getName();
+    robot->getName();
 
     return EXIT_SUCCESS;
  }
