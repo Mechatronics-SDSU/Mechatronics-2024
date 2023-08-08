dad24497763b70835bed0373c8b7db7ecba3c503
mechatronics <mechatronics.sdsu@gmail.com>
2023-08-08 10:24:13 -0700

    Working On Robot Inheritance

--- src/ros2_ws/src/robot/src/main.cpp
+++ src/ros2_ws/src/robot/src/main.cpp
@@ -1,25 +1,19 @@
 #include "robot.hpp"
-#include "junebug.hpp"
-#include "percy.hpp"
 #define CONFIG_FILE "/home/mechatronics/clean/src/ros2_ws/src/robot/config.json"
 
-namespace 
-{
-    void registerRobots()
-    {
-        RobotFactory::RegisterRobotType(RobotType::Percy, &Percy::CreatePercy);
-        RobotFactory::RegisterRobotType(RobotType::Junebug, &Junebug::CreateJunebug);
-    }
-}
+std::unordered_map<std::string, RobotType> RobotFactory::robot_types = {};
+std::unordered_map<RobotType, std::function<std::unique_ptr<Robot>(const Configuration&)>> RobotFactory::constructors = {};
 
-std::unordered_map<RobotType, RobotConstructor> RobotFactory::constructors = {};
 int main(int argc, char** argv)
 {
-    registerRobots();
+    RobotFactory::registerRobots();
     std::unique_ptr<Configuration> config = std::make_unique<Configuration>(CONFIG_FILE);
     nlohmann::json json_string = config->getJsonString();
-    RobotType type = (json_string["robot"] == "percy") ? RobotType::Percy : RobotType::Junebug;
+
+    RobotType type = RobotFactory::getType(json_string["robot"]);
     std::unique_ptr<Robot> robot = RobotFactory::CreateRobot(type, *config);
-    robot->getName();
+    
+    robot->
+
     return EXIT_SUCCESS;
  }
