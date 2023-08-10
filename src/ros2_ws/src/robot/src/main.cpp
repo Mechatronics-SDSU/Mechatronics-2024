d6749535bd04a1fc28fdf66237b722b8949f7e1c
mechatronics <mechatronics.sdsu@gmail.com>
2023-08-09 22:55:34 -0700

    Organize main to have init function - Zix

--- src/ros2_ws/src/robot/src/main.cpp
+++ src/ros2_ws/src/robot/src/main.cpp
@@ -1,28 +1,21 @@
 #include "robot.hpp"
 #include "robot_factory.hpp"
-#include "mainwindow.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include <filesystem>
-#include <iostream>
-#include <signal.h>
 
-/* CONFIG_PATH defined in CMakeLists.txt*/
+std::shared_ptr<Robot> initRobot(int argc, char* argv[])
+{
+    RobotFactory::registerRobots();
+    std::unique_ptr<Configuration> config = std::make_unique<Configuration>(CONFIG_PATH); // defined in CMakeLists.txt
+    nlohmann::json json_string = config->getJsonString();
+    RobotType type = RobotFactory::getType(json_string["robot"]);
+    return RobotFactory::CreateRobot(type, *config);
+}
 
+/* Robot entry point */
 int main(int argc, char** argv)
 {
-    /* Init Config*/
     rclcpp::init(argc, argv);
-    RobotFactory::registerRobots();
-    std::unique_ptr<Configuration> config = std::make_unique<Configuration>(CONFIG_PATH);
-    nlohmann::json json_string = config->getJsonString();
-
-    /* Construct Robot*/
-    RobotType type = RobotFactory::getType(json_string["robot"]);
-    std::shared_ptr<Robot> robot = RobotFactory::CreateRobot(type, *config);
-
-    /* Async Run Read-Only Nodes*/
-
-    /* Run Loop For Write Thread*/
+    std::shared_ptr<Robot> robot = initRobot(argc, argv);
     rclcpp::spin(robot);
     rclcpp::shutdown();   
     return EXIT_SUCCESS;
