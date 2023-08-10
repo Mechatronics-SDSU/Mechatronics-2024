2eca21679130b8f23c0fc5d17af75bf0867ae7fa
mechatronics <mechatronics.sdsu@gmail.com>
2023-08-09 22:58:28 -0700

    update component.cpp with enable functions - Zix

--- src/ros2_ws/src/robot/src/component.cpp
+++ src/ros2_ws/src/robot/src/component.cpp
@@ -1,2 +1,16 @@
 #include "component.hpp"
 
+void Component::enable()
+{
+    this->enabled = true;
+}
+
+void Component::disable()
+{
+    this->enabled = false;
+}
+
+void Component::validate()
+{
+    this->validated = true;
+}
