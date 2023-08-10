669d19f8e17b40d93e39320ca2fc649948502062
mechatronics <mechatronics.sdsu@gmail.com>
2023-08-10 01:37:09 -0700

    Fix Insane Link error - Zix

--- src/ros2_ws/src/gui_listener/src/gui_listener_node.cpp
+++ src/ros2_ws/src/gui_listener/src/gui_listener_node.cpp
@@ -1,9 +1,9 @@
-#include "gui_listener.hpp"
+#include "gui_listener_node.hpp"
 
 GUI_Listener::GUI_Listener() : Node("gui_listener")
 {
     gui_subscriber = this->create_subscription<std_msgs::msg::String>("gui_data", 10, [this](const std_msgs::msg::String::SharedPtr msg)
     {
-        RCLCPP_INFO(msg->data);
+        RCLCPP_INFO(this->get_logger(), "%s", msg->data.c_str());
     });
 }
