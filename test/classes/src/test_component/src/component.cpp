c08e214ceb7b299eaca287661ed553edf8037316
mechatronics <mechatronics.sdsu@gmail.com>
2023-08-19 15:26:29 -0700

    Some Structural Changes

---
+++ test/classes/src/test_component/src/component.cpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include <chrono>
+
+class _TEST_SUITE : public ::testing::Test {
+public:
+    static void SetUpTestSuite() {
+    }
+
+    static void TearDownTestSuite() {
+    }
+};
+
+namespace
+{
+    
+}
+
+TEST_F(_TEST_SUITE, test_component)
+{
+    
+}
