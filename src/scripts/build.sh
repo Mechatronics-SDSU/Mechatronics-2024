c062d489338ffb35c3e8fa8d2ab6b1fa25bc86e1
mechatronics <mechatronics.sdsu@gmail.com>
2023-08-20 00:34:11 -0700

    Modify Build

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -2,16 +2,16 @@
 
 cd ../classes
 build scion_types
+
 . s
 build dependency_library
 
 cd ../ros2_ws
 build robot_library
-. s
+colcon build
 
+. s
 cd ../../test/ros2_ws
 build test_library
-. s
 
-cd ../../src/scripts/src
-build can_listener
+. s
