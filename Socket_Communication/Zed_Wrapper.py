e46731fda25cfb7ab1ed6b6cf0002896a64e7977
Mechatronics <mechatronics.sdsu@gmail.com>
2023-11-01 16:54:27 -0700

    added depth sensing per object, and visualization

--- Socket_Communication/Zed_Wrapper.py
+++ Socket_Communication/Zed_Wrapper.py
@@ -1,6 +1,9 @@
 import pyzed.sl as sl
+import math
 import cv2
+import numpy as np
 import copy
+import statistics
 
 class Zed:
     
@@ -9,6 +12,7 @@ class Zed:
         self.init_params = sl.InitParameters()
         self.init_params.camera_resolution = sl.RESOLUTION.HD720
         self.init_params.camera_fps = 60
+        #self.init_params.depth_mode = sl.DEPTH_MODE.NEURAL
 
     def open(self):
         state = self.zed.open(self.init_params)
@@ -19,7 +23,6 @@ class Zed:
         return frame
 
     def get_image(self):
-        image = None
         image_zed = sl.Mat()
         if self.zed.grab() == sl.ERROR_CODE.SUCCESS:
             self.zed.retrieve_image(image_zed, sl.VIEW.RIGHT)
@@ -30,29 +33,51 @@ class Zed:
         image_zed = sl.Mat()
         if (self.zed.grab() == sl.ERROR_CODE.SUCCESS):
             self.zed.retrieve_image(image_zed, sl.VIEW.DEPTH)
-            return copy.deepcopy(image_zed.get_data())
+            image = image_zed.get_data()
+            return copy.deepcopy(image)
         
-    def get_min_depth(self, x1, y1, x2, y2):
+    def get_median_depth(self, x1, y1, x2, y2):
+        width = self.zed.get_camera_information().camera_resolution.width
+        height = self.zed.get_camera_information().camera_resolution.height
+
+        if x1 > width or x2 > width:
+            return -1
+        elif y1 > height or y2 > height:
+            return -1
+        elif self.zed.grab() != sl.ERROR_CODE.SUCCESS :
+            return -1
+        
         depth_zed = sl.Mat(self.zed.get_camera_information().camera_resolution.width, 
-                           self.zed.get_camera_information().camera_resolution.height, 
-                           sl.MAT_TYPE.F32_C1)
+                            self.zed.get_camera_information().camera_resolution.height, 
+                            sl.MAT_TYPE.F32_C1)
         
-        if self.zed.grab() == sl.ERROR_CODE.SUCCESS :
-            # Retrieve depth data (32-bit)
-            self.zed.retrieve_measure(depth_zed, sl.MEASURE.DEPTH)
-            # Load depth data into a numpy array
-            depth_ocv = depth_zed.get_data()
-            # Print the depth value at the center of the image
-            print(depth_ocv[int(len(depth_ocv)/2)][int(len(depth_ocv[0])/2)])
-        
+        # Retrieve depth data (32-bit)
+        self.zed.retrieve_measure(depth_zed, sl.MEASURE.DEPTH)
+        # Print the depth value at the center of the image
+        depth_list = list()
+        skip_size = 5
+        for x in range(x1, x2 - (skip_size), skip_size):
+            for y in range(y1, y2 - (skip_size), skip_size):
+                _, depth = depth_zed.get_value(x, y)
+                if not math.isnan(depth) and math.isfinite(depth):
+                    depth_list.append(int(depth))
+
+        if not depth_list:
+            return -1
+        median = statistics.median_low(depth_list)
+        #print(median)
+        return median
+    
 
         
 def main():
-    z = Zed()
+    zed = Zed()
+    state = zed.open()
     while True:
-        image = z.get_image()
-        cv2.imshow("image_test", image)
-        cv2.waitKey(1)
+        image = zed.get_depth_image()
+        if (image is not None):
+            cv2.imshow("image_test", image)
+            cv2.waitKey(1)
         continue
 
 if __name__ == '__main__':
--- Socket_Communication/run_video_over_socket.py
+++ Socket_Communication/run_video_over_socket.py
@@ -1,12 +1,13 @@
 import Yolov5Detection as yv5
 import Socket_Client
 import argparse
+import math
 import cv2
 import copy
 from Zed_Wrapper import Zed
 
 TARGET_SIZE = 640
-show_depth = False
+show_depth = True
 get_depth = True
 
 try:
@@ -19,19 +20,31 @@ parser.add_argument('-host_ip', help='ip to send images to', required=False)
 parser.add_argument('-port', help='port to send images over', required=False)
 parser.add_argument('-show_boxes',help='boolean to show object detection boxes', required=False)
 parser.add_argument('-model_name', help='model to run on', required=False)
-#parser.add_argument('-show_depth', help='show depth map', required=False)
-#parser.add_argument('-get_depth, help='get depth map', required=False)
+parser.add_argument('-show_depth', help='show depth map', required=False)
+parser.add_argument('-get_depth', help='get depth map', required=False)
 args = parser.parse_args()
 
 def get_image_from_webcam(cap):
     ret, frame = cap.read()
     return frame
 
+def get_nearest_object(results, zed):
+    nearest_object = math.inf
+    for box in results.xyxy[0]:
+        if box[5] == 0:
+            median = zed.get_median_depth(int(box[0]), int(box[1]), int(box[2]), int(box[3]))
+            nearest_object = min(median, nearest_object)
+            
+    return nearest_object
+
+
 def parse_arguments():
     host = args.host_ip
     port = args.port
     show_boxes = args.show_boxes
     model_name = args.model_name
+    get_depth = args.get_depth
+    show_depth = args.show_depth
 
     if host is None:
         host = '127.0.0.1'
@@ -44,15 +57,25 @@ def parse_arguments():
     else:
         show_boxes = False
 
+    if get_depth is None or get_depth == 'True':
+        get_depth = True
+    else:
+        get_depth = False
+
+    if show_depth is None or show_depth == 'False':
+        get_depth = False
+    else:
+        get_depth = True
+
     if model_name is None:
         model_name = './models_folder/yolov5m.pt'
     else:
         model_name = "./models_folder/" + model_name
 
-    return host, port, show_boxes, model_name
+    return host, port, show_boxes, model_name, get_depth, show_depth
 
 def main():
-    host, port, show_boxes, model_name = parse_arguments()
+    host, port, show_boxes, model_name, get_depth, show_depth = parse_arguments()
 
     socket = Socket_Client.Client(host, port)
     socket.connect_to_server()
@@ -61,6 +84,7 @@ def main():
     zed = None
     cap = None
     state = None
+    depth = 0
 
     zed = Zed()
     state = zed.open()
@@ -73,7 +97,6 @@ def main():
     while True:
         if zed is not None:
             image = zed.get_image()
-            zed.get_min_depth(0, 0, 0, 0)
         elif cap is not None:
             image = get_image_from_webcam(cap)
         else:
@@ -82,11 +105,14 @@ def main():
 
         results = detection.detect_in_image(image)
 
+        if zed is not None and get_depth:
+            depth = get_nearest_object(results, zed)
         if show_depth:
-            image = zed.get_depth_image
+            image = zed.get_depth_image()
         if show_boxes:
             detection.draw_boxes(image, results)
             detection.draw_lines(image, results)
+        print(depth)
         
         try:
             socket.send_video(image)
